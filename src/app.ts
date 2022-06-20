3430d818b3dc27136324bad72a2c16f0702ecf6f
Dylan Schoop <42898108+CurleyDS@users.noreply.github.com>
2022-06-20 15:54:03 +0200

    Merged prototypes

--- src/app.ts
+++ src/app.ts
@@ -2,14 +2,13 @@
 import * as PIXI from 'pixi.js'
 import pizzaImage from './images/pizza.png'
 import sauceImage from './images/sauce.png'
-import pepperoniPizza from "./images/pepperoni.png"
-import bottomPizza from "./images/bottom.png"
 import { Pizza } from "./pizza"
 import { Sauce } from "./sauce"
 
 export class Game {
 
 	pixi:PIXI.Application;
+	resetButton:any;
 	pizza:Pizza;
 	sauce:Sauce[] = [];
 	drawPosition:any = null;
@@ -21,11 +20,11 @@ export class Game {
 		this.pixi = new PIXI.Application({ width: 800, height: 450, });
 		document.body.appendChild(this.pixi.view);
 
+		this.resetButton = document.getElementById('reset');
+
 		// preload all the textures
 		this.loader = new PIXI.Loader();
 		this.loader
-      .add('pepperoniTexture', pepperoniPizza)
-      .add('bottomTexture', bottomPizza)
 			.add('pizzaTexture', pizzaImage) // laadt de images in de variabelen uit de import
 			.add('sauceTexture', sauceImage); // laadt de images in de variabelen uit de import
 		this.loader.load(() => this.loadCompleted());
@@ -50,23 +49,6 @@ export class Game {
 				this.drawingStarted = true;
 			}
 		}
-    
-    let pepperoni = new PIXI.Sprite(loader.resources["pepperoniTexture"].texture!)
-    pepperoni.anchor.set(0.5);
-    pepperoni.width = pixi.screen.height / 2;
-    pepperoni.height = pixi.screen.height / 2;
-    pepperoni.position.set(pixi.screen.width / 2, pixi.screen.height / 2);
-    pepperoni.interactive = true;
-
-    let bottom = new PIXI.Sprite(loader.resources["bottomTexture"].texture!)
-    bottom.anchor.set(0.5);
-    bottom.width = pixi.screen.height / 2;
-    bottom.height = pixi.screen.height / 2;
-    bottom.position.set(pixi.screen.width / 2, pixi.screen.height / 2);
-    bottom.interactive = true;
-    
-    pixi.stage.addChild(pepperoni);
-    pixi.ticker.add((delta) => update(delta))
 
 		const onMove = (e:any) => {
 			if (this.drawingStarted) {
@@ -95,19 +77,8 @@ export class Game {
 		this.pizza.on('mouseup', onUp);
 		this.pizza.on('touchend', onUp);
 
-		this.pixi.ticker.add(() => this.addSauce());
+		this.pixi.ticker.add((delta) => this.addSauce(delta));
 	}
-  
-  function update(delta:number) {
-    pepperoni.y -= 5 * delta
-    if (pepperoni.y < -600) {
-      pixi.stage.removeChild(pepperoni)
-      if (pixi.stage.children.length == 0){
-        pixi.stage.addChild(bottom);
-        console.log(`is dat zo dylan?`)
-      }
-    }
-  }
   
 	insideBorder(position:any) {
 		if (position != null) {
@@ -126,12 +97,35 @@ export class Game {
 		}
 	}
 
-	addSauce() {
+	resetPizza() {
+		if (this.resetButton.checked) {
+			return true;
+		}
+		return false;
+	}
+
+	addSauce(delta:number) {
 		if (this.insideBorder(this.drawPosition)) {
 			if (this.drawingStarted) {
 				let sauce = new Sauce(this.loader.resources["sauceTexture"].texture!, this.drawPosition)
 				this.pixi.stage.addChild(sauce);
 				this.sauce.push(sauce);
+			}
+		}
+		
+		if (this.resetPizza()) {
+			this.pizza.y -= 5 * delta
+			if (this.pizza.y < -600) {
+				this.pixi.stage.removeChild(this.pizza)
+				if (this.pixi.stage.children.length == 0){
+					this.pizza = new Pizza(
+						this.loader.resources["pizzaTexture"].texture!,
+						this.pixi.screen.width,
+						this.pixi.screen.height
+					);
+					this.pixi.stage.addChild(this.pizza);
+					this.resetButton.checked = false;
+				}
 			}
 		}
 	}
