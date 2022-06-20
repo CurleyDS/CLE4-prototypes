4a488f87e5498a0148820ece2b6f4dd4dda207f4
Dane Koenders <dane.koenders@gmail.com>
2022-06-20 13:02:17 +0200

    v1.15

--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,10 @@ import * as PIXI from 'pixi.js'
 
 // create a pixi canvas
 const pixi = new PIXI.Application({ width: 800, height: 450 })
-document.body.appendChild(pixi.view)
+const pixiCanvas = document.getElementById("pixi-canvas")
+if (pixiCanvas != null) {
+    pixiCanvas.appendChild(pixi.view)
+}
 
 // preload all the textures
 const loader = new PIXI.Loader()
@@ -12,6 +15,6 @@ loader.load(()=>loadCompleted())
 
 // after loading is complete
 function loadCompleted() {
-    let  = new PIXI.Sprite(loader.resources[""].texture!) // zet image om in een sprite en zet deze in een variabele
+    let completed = new PIXI.Sprite(loader.resources[""].texture!) // zet image om in een sprite en zet deze in een variabele
     pixi.stage.addChild() // voeg variabele toe aan het pixi canvas
 }
